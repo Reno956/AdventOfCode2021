17687cbf67368a387033f60089680c0ec8e2cef6
Renato Padilla <renato.padilla@epn.edu.ec>
2021-12-01 10:04:57 -0500

    Day1

---
+++ Day1/sonar_sweep.py
@@ -0,0 +1,19 @@
+def sonar(data):
+    j=0
+    for i in range(len(data)-1):
+        if(data[i+1]>data[i]):
+            j+=1
+    return j
+
+def read():
+    data = []
+    f = open ('data.txt','r')
+    text = f.readlines()
+    for line in text:
+        data.append(int(line.strip()))
+    f.close()
+    return data
+
+#count = sonar([199,200,208,210,200,207,240,269,260,263])
+count = sonar(read())
+print(count)
---
+++ Day1/sonar_sweep1.py
@@ -0,0 +1,26 @@
+def sonar(data):
+    j=0
+    for i in range(len(data)-1):
+        if(data[i+1]>data[i]):
+            j+=1
+    return j
+
+def read():
+    data = []
+    f = open ('data.txt','r')
+    text = f.readlines()
+    for line in text:
+        data.append(int(line.strip()))
+    f.close()
+    return data
+
+def addition(data):
+    dataS = []
+    for i in range(len(data)-2):
+        sum = data[i]+data[i+1]+data[i+2]
+        dataS.append(sum)
+    return dataS
+
+#count = sonar([199,200,208,210,200,207,240,269,260,263])
+count = sonar(addition(read()))
+print(count)
